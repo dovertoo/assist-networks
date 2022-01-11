9f573199def17d4bd8d22eb1028f6352df8fd067
Dan <dovertoomjr@gmail.com>
2022-01-10 21:16:02 -0500

    MD-Hop assist network created

--- md_mlax_2021_assist_network.R
+++ md_mlax_2021_assist_network.R
@@ -1,11 +1,14 @@
 library(janitor)
 library(tidyverse)
 library(igraph)
+library(ggraph)
 
 
 player_data <- read.csv("data/md_player_data_2021.csv") %>% 
      clean_names() %>% 
      rename(player_id = id) %>% 
+     separate(player, into = c("player_first", "player_last")) %>% 
+     mutate(player = paste0(str_sub(player_first, start = 1, end = 1), ". ", player_last)) %>% 
      select(player, player_id)
 games_data <- read.csv("data/md_games_data_2021.csv") %>% 
      clean_names() %>% 
@@ -20,29 +23,80 @@ assist_data <- raw_assist_data %>%
      rename(assister = player) %>% 
      filter(game_id == 9206) #md vs hopkins on 3/6/2021
 
+#total number of assists each player had 
+assists <- assist_data %>% 
+        select(assister) %>% 
+        count(assister) %>% 
+        rename(num_assists = n) 
+
 nodes <- assist_data %>% 
      select(assister, shooter) %>% #need to have all shooters/assisters listed as vertices
      pivot_longer(everything(), values_to = "assister_shooter") %>% 
      select(-name) %>% 
-     distinct()
+     count(assister_shooter) %>% 
+     rename(num_interactions = n) %>% 
+     left_join(., assists, by = c("assister_shooter" = "assister")) %>% 
+     mutate(num_assists = replace_na(num_assists, 0)) %>% 
+     distinct() %>% 
+     mutate(player_name = assister_shooter)
+
+assister_shooter_combo <- assist_data %>% 
+        select(assister, shooter) %>% 
+        count(assister, shooter) %>% 
+        rename(assister_shooter_freq = n) %>% 
+        filter(assister_shooter_freq > 1)
 
 links <- assist_data %>% 
-     select(assister, shooter) #define all connections between assisters and shooters
-
+     select(assister, shooter) %>% #define all connections between assisters and shooters
+     left_join(., assister_shooter_combo, by = c("assister", "shooter")) %>% 
+     mutate(assister_shooter_freq_label = replace_na(assister_shooter_freq, "")) %>% 
+     mutate(assister_shooter_freq = replace_na(assister_shooter_freq, 0))
+        
 net <- graph_from_data_frame(d=links, vertices=nodes, directed=T) 
 
 #set vertex size based on number of interactions
 deg <- degree(net, mode="all")
 V(net)$size <- deg*4
 
-# Set edge width based on weight:
-E(net)$width <- E(net)$weight
+# Set edge width based on number of assists
+E(net)$width <- E(net)$num_assists
 
+#kk best layout so far - gives consistent layout when saved to a variable
+#l <- layout_with_kk(net)
 
 l <- layout_with_kk(net)
 
 #need to play with layouts??
-plot(net, vertex.label.degree = pi/2, vertex.label.dist = 3, layout = l) #layout.fruchterman.reingold
+plot(net, vertex.label.degree = pi+0.85, 
+     vertex.label.dist = 3.6, 
+     layout = l,
+     main = "Maryland Assist Network",
+     sub = "Hopkins vs Maryland - March 6th, 2021") 
 
 
+md_hop_net <- ggraph(net, layout = "kk") +
+     geom_edge_arc(arrow = arrow(length = unit(5, 'mm'), type = "closed"), 
+                      start_cap = circle(2, 'mm'),
+                      end_cap = circle(4, 'mm'),
+                      strength = 0.15,
+                      aes(label = assister_shooter_freq_label,
+                          edge_width = assister_shooter_freq,
+                          edge_color = "gray25"),
+                      show.legend = FALSE) +
+     geom_node_point(aes(size = num_assists + 50),
+                     shape = 21, fill = "black",
+                     show.legend = FALSE) +   # add nodes to the plot
+     geom_node_text(aes(label = player_name), size=5, color="gray50",
+                    repel=T, nudge_x = 0.175, nudge_y = 0.14) +
+     labs(caption = "Data: @laxreference | Plot: @danovertoom",
+          title = "Maryland Assist Network",
+          subtitle = "Hopkins vs Maryland - March 6th, 2021") +
+     scale_edge_width_continuous(range = c(1.5,3)) +
+     scale_size_continuous(range = c(4,12)) +
+     theme_void() +
+     theme(plot.title = element_text(size=22),
+           plot.subtitle = element_text(size=14))
 
+md_hop_net
+
+ggsave(filename = "plots/md_hop_net_test.png", plot = md_hop_net, dpi = 800)
